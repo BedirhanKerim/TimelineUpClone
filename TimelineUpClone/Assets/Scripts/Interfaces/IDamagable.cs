0cb2f94cdbefaf737d3e10a58aa6155683c25405
Bedirhan Kerim <bedirhankerim07@gmail.com>
2025-03-11 13:40:45 +0300

    chore: Added UniTask, structured folders, implemented interfaces, and added utility script

---
+++ TimelineUpClone/Assets/Scripts/Interfaces/IDamagable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public interface IDamagable
+{
+    void TakeDamage(float damage);
+  //  bool IsAlive();
+
+
+}    
+
---
+++ TimelineUpClone/Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    /// <summary>
+    /// This bool determines that this singleton is "DontDestroyOnLoad" or not
+    /// </summary>
+    [SerializeField] private bool _dontDestroy = false;
+
+    static T m_Instance;
+    public static T Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = FindObjectOfType<T>();
+            }
+            return m_Instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (m_Instance == null)
+        {
+            m_Instance = this as T;
+            if (_dontDestroy)
+            {
+                transform.parent = null;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+        else
+        {
+            if (m_Instance != this as T)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
